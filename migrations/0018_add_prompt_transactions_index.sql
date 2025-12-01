f003b7ea120cc718bc92e869c65aa3c6d3bcd541
cyril <cyril.mawutor@gmail.com>
2025-12-01 19:22:50 +0000

    Use a composite index for performance. Closes #13

--- migrations/0018_add_prompt_transactions_index.sql
+++ migrations/0018_add_prompt_transactions_index.sql
@@ -1,2 +1,1 @@
-CREATE INDEX IF NOT EXISTS idx_prompt_transactions_user_id ON prompt_transactions(user_id);
-CREATE INDEX IF NOT EXISTS idx_prompt_transactions_created_at ON prompt_transactions(created_at);
+CREATE INDEX IF NOT EXISTS idx_prompt_transactions_user_id_created_at ON prompt_transactions(user_id, created_at);
